f75f3ac3824513875d24f2f397166b5e1239cda5
“Thais <thais.vnunes@hotmail.com>
2017-11-11 14:34:30 -0200

    Enviando atualização

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3x9ly5utal0zp#0^c^5fay%bvj_kdmeimy^0^-@btqj$k9$o_x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thaisnunes.pythonanywhere.com']
 
 
 # Application definition
